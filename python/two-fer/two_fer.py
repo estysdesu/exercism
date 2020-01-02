c93cec6f6c4ea9463f79695a625e728d484f7759
Tyler Estes <estysdesu@gmail.com>
2020-01-02 12:46:34 -0500

    more pythonic

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,7 +1,2 @@
-def two_fer(*name):
-    if len(name) > 1:
-        raise Exception("only one name can be passed")
-    elif len(name) == 1:
-        return f"One for {name[0]}, one for me."
-    else:
-        return "One for you, one for me."
+def two_fer(name="you"):
+    return f"One for {name}, one for me."
