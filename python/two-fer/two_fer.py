6544903ee28294711feaa4ab823586e5e7880c6b
Tyler Estes <estysdesu@gmail.com>
2020-01-02 12:44:41 -0500

    python/two-fer

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,2 +1,7 @@
-def two_fer(name):
-    pass
+def two_fer(*name):
+    if len(name) > 1:
+        raise Exception("only one name can be passed")
+    elif len(name) == 1:
+        return f"One for {name[0]}, one for me."
+    else:
+        return "One for you, one for me."
