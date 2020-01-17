00e6ae6d988a4fcca80d2dc407303117294e4749
Tyler Estes <estysdesu@gmail.com>
2020-01-17 13:33:22 -0500

    rust/atbash-cipher: end for class

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -16,12 +16,35 @@ const ENCODE_GROUP_LENGTH: usize = 5;
 
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
+    // panic if invalid input
     if !plain.is_ascii() {
         panic!("only ascii strings are encodable")
     }
-    plain.make_ascii_lowercase(); // inplace
-    let mut encoded = String::with_capacity(plain.len() * (1 + 1 / ENCODE_GROUP_LENGTH)); // this includes punctuation room -- keep?
-    for c in plain.chars() { // since only ascii, no surprises (runes unnecessary)
+    // make String variable with max possible capacity needed (but, possibly more than needed b/c punctuation is dropped)
+    let mut encoded = String::with_capacity(plain.len() * (1 + 1 / ENCODE_GROUP_LENGTH));
+    // iterate over chars; since only ascii, no surprises (rune conversion not necessary)
+    for c in plain.to_ascii_lowercase().chars() {
+        // handle if group of 5 exists (push space)
+        if encoded.len() != 0 && encoded.len() % 5 == 0 {
+            encoded.push(' ');
+        }
+
+        if c.is_numeric() {
+            encoded.push(c)
+        }
+        // c.alphabetical()
+        let i = ALPHABET.find(c);
+        // handle non alphabetical chars (punctuation, etc.)
+        if i.is_none() {
+            continue;
+        }
+        // push complimentary character
+        encoded.push(
+            ALPHABET
+                .chars()
+                .nth(ALPHABET.len() - i.unwrap() - 1)
+                .unwrap(),
+        );
     }
     encoded.shrink_to_fit();
     encoded
@@ -29,7 +52,7 @@ pub fn encode(plain: &str) -> String {
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    if !plain.is_ascii() {
+    if !cipher.is_ascii() {
         panic!("only ascii strings are decodable")
     }
 
