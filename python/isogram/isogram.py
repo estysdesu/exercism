0c95c6cce213e63bf321165c35889cb736d1b7c8
Tyler Estes <estysdesu@gmail.com>
2020-02-03 09:45:37 -0500

    python/isogram iteration 2

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,4 +1,4 @@
 def is_isogram(phrase: str) -> bool:
-    """Determine if a string/phrase (multiple/dashes allowed) is an isogram."""
-    filtered_phrase = phrase.replace(" ", "").replace("-", "").lower()
-    return False if len(set(filtered_phrase)) < len(filtered_phrase) else True
+    """Determine if a string/phrase (multiple spaces/dashes allowed) is an isogram."""
+    filtered = tuple(filter(str.isalpha, phrase.lower()))
+    return False if len(set(filtered)) < len(filtered) else True
