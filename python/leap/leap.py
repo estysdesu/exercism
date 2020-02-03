714b5ffd7c6c7f6edf181725f6e595b4b48c403c
Tyler Estes <estysdesu@gmail.com>
2020-02-03 13:03:26 -0500

    python/leap iteration 2

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,13 +1,4 @@
 def leap_year(year: int) -> bool:
     """Determine if a year number (4 digit integer) is a leap year."""
 
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
+    return year % 4 == 0 if (year % 100 != 100 or year % 400 == 0)
