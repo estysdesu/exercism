b2e0991c2b0efadd2d4a2753a255785f151ab4b5
Tyler Estes <estysdesu@gmail.com>
2020-03-18 15:31:11 -1000

    rust/atbash-cipher iteration 2: refactor

---
+++ python/twelve-days/twelve_days.py
@@ -0,0 +1,37 @@
+from typing import Tuple, List
+
+NTH: Tuple = (
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+    "eleventh",
+    "twelfth",
+)
+PHRASE: Tuple = (
+    "twelve Drummers Drumming, ",
+    "eleven Pipers Piping, ",
+    "ten Lords-a-Leaping, ",
+    "nine Ladies Dancing, ",
+    "eight Maids-a-Milking, ",
+    "seven Swans-a-Swimming, ",
+    "six Geese-a-Laying, ",
+    "five Gold Rings, ",
+    "four Calling Birds, ",
+    "three French Hens, ",
+    "two Turtle Doves, ",
+    "and a Partridge in a Pear Tree.",
+)
+
+
+def recite(st_verse: int, end_verse: int) -> List[str]:
+    return [""]
+
+
+# print(recite(2, 7))
